7216db3efdb91cb908b4d36b8c0ac768fd1d5827
Spansos <spaansstan@gmail.com>
2022-11-21 15:07:15 +0100

    Message

--- project.py
+++ project.py
@@ -1,3 +1,6 @@
+def check_antwoord(antwoord):
+    return antwoord == 42
+
 def hello_world(lang):
     if lang == "NL":
         print("Hallo wereld!")
@@ -7,3 +10,9 @@ def hello_world(lang):
         print("Hello world!")
 
 hello_world("EN")
+
+antwoord = input("Wat is het antwoord op de ultieme vraag van het leven, het universum, en alles?")
+if check_antwoord(antwoord):
+    print("Correct!")
+else:
+    print("Mogelijk heb je een andere vraag in je hoofd?")
